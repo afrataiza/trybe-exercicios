1cbf29dbe6d605fdf479c31f7589c943d0916782
Afra Taiza <afrataiza@gmail.com>
2022-02-12 12:40:30 -0300

    Adiciona exercicio do dia

--- fundamentos/bloco-04-introducao-JS-e-logica-de-programacao/dia-01-JS-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-JS-e-logica-de-programacao/dia-01-JS-primeiros-passos/scripts.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-console */
 const myName = 'Afra Taíza';
 const birthCity = 'Aracaju';
 let birthYear = 1986;
 let birthMonth = 4;
-let actualYear = 2022
+let actualYear = 2022;
 let date = new Date();
-let actualMonth = date.getMonth() + 1
+let actualMonth = date.getMonth() + 1;
 
-console.log (myName);
-console.log (birthCity);
-console.log (birthYear);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
 if (birthMonth < actualMonth) {
-    console.log('Você tem', (actualYear - birthYear), 'anos!');
+  console.log('Você tem', actualYear - birthYear, 'anos!');
 } else {
-    console.log('Você tem', (actualYear - birthYear - 1), 'anos!');
+  console.log('Você tem', actualYear - birthYear - 1, 'anos!');
 }
 
